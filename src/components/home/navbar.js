9c847ee685eb1f955d17dfcb8099deb48b4f7ffb
unclechuen <33083306+unclechuen@users.noreply.github.com>
2022-04-13 17:26:30 +0800

    Minor

--- src/components/home/navbar.js
+++ src/components/home/navbar.js
@@ -12,6 +12,8 @@ import {
 
 import { Plus } from 'tabler-icons-react';
 
+import "../../styles/navbar.css"
+
 
 const NavBar = () => {
     const [hovered,setHovered] = useState(false)
@@ -19,13 +21,7 @@ const NavBar = () => {
         width: "100%",
         height:"100%",
         textAlign: "left",
-        paddingLeft: 30,
-        paddingRight: 30,
-        // // backgroundColor:"#e7f5ff",
-        
-        // (hovered) ? "backgroundColor:'#e7f5ff'":""
-            
-        
+        padding: 15,
     }
 
     return (
@@ -44,18 +40,23 @@ const NavBar = () => {
                     <Text align="left">
                         Good: Gold <br />
                         Supplier: Johnny Co. <br />
-                        Invoices<br />
+                        Distributor: Oscar Co.<br />
+                        Status: On the way<br />
                     </Text>
                 </Button>
             </Navbar.Section>
 
             <Navbar.Section >
                 <Button variant='subtle' style={buttonStyle}>
-                    <Text >
-                        Good: Gold <br />
-                        Supplier: . <br />
-                        Invoices<br />
-                    </Text>
+                    <table>
+                        <tr>
+                            <th>Good</th>
+                            <td>Jewel</td>
+                        </tr>
+                        {/* Supplier: Johnny Co. <br />
+                        Supplier: Ivan Co. <br />
+                        Status: On the way<br /> */}
+                    </table>
                 </Button>
             </Navbar.Section>
 
