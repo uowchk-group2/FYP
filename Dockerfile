69d9ff8a3896d4e3adaa4b07168edc9a00723534
unclechuen <33083306+unclechuen@users.noreply.github.com>
2022-06-01 00:51:03 +0800

    Build next project in Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,5 @@ RUN npm config set legacy-peer-deps true
 RUN npm install
 COPY . .
 EXPOSE 3000
+RUN npm run build
 CMD ["npm","start"]
--- src/functions/user.js
+++ src/functions/user.js
@@ -8,20 +8,25 @@ export const login = () => {
     }
 
     let body = {
-        username: "johnny",
+        username: "johnny1",
         password: "johnny"
     }
 
-    try {
-        
-        axios.post('https://tomcat.johnnyip.com/fyp-backend/login', body, config)
-            .then((response) => {
-                console.log(response)
-            })
+    axios.post('https://tomcat.johnnyip.com/fyp-backend/login', body, config)
+        .then((response) => {
+            console.log(response)
+            console.log(response.status)
+            console.log(response.data)
+        })
+        .catch((err) => {
+            console.log(err)
+            if (err.code === "ERR_NETWORK") {
+                console.log("Server Error")
+            }else if (err.code === "ERR_BAD_REQUEST") {
+                console.log("Wrong Credential")
+            }
+        })
 
-    }catch (err) {
-        console.log(err)
-    }
 
 
     // fetch('https://tomcat.johnnyip.com/fyp-backend/login', requestOptions)
