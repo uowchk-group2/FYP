e5716f5be0dce9868d4db49a58986bdc8568d3b4
unclechuen <33083306+unclechuen@users.noreply.github.com>
2022-06-08 03:52:58 +0800

    Update map.js

--- src/components/delivery/detail/map.js
+++ src/components/delivery/detail/map.js
@@ -27,6 +27,14 @@ const greenMark = {
     rotation: 0,
     scale: 1.5,
 };
+const redMark = {
+    path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+    fillColor: "red",
+    fillOpacity: 1,
+    strokeWeight: 0.0,
+    rotation: 0,
+    scale: 1.5,
+};
 
 
 const RouteMap = (props) => {
@@ -76,29 +84,43 @@ const RouteMap = (props) => {
                 <GoogleMap
                     mapContainerStyle={{ width: '100%', height: '400px' }}
                     center={{ lat: chosenCheckpoint.lat, lng: chosenCheckpoint.lng }}
+                    clickableIcons={false}
+                    options={{
+                        styles: [
+                            {
+                                featureType: "poi",
+                                elementType: "labels",
+                                stylers: [
+                                    { visibility: "off" }
+                                ]
+                            }
+                        ]
+                    }}
                     zoom={15}
                 >
                     {[...checkpoints].map((item, i) => {
                         console.log(item)
-                        return <Marker
-                            icon={(item.arrivalActual != null) ? greenMark : grayMark}
-                            position={{ lat: item.lat, lng: item.lng }}
-                            onClick={() => { changeInfoWindow(item) }} >
-                            {
-                                (item.title === chosenCheckpoint.title) ?
-                                    <InfoWindow
-                                        position={{ lat: chosenCheckpoint.lat, lng: chosenCheckpoint.lng }}
-                                    >
-                                        <div style={divStyle}>
-                                            <h2>Next: {chosenCheckpoint.title}</h2>
-                                            <h3>Estimated Arrival time: {convertToTimeString(chosenCheckpoint.arrivalExpected)}</h3>
-                                            <h3>Distance to go: {chosenCheckpoint.prevDistance / 1000} km</h3>
-                                        </div>
-                                    </InfoWindow>
-                                    : <></>
-                            }
+                        return (
+                            <Marker
+                                icon={(item.arrivalActual != null) ? greenMark : grayMark}
+                                position={{ lat: item.lat, lng: item.lng }}
+                                onClick={() => { changeInfoWindow(item) }} >
+                                {
+                                    (item.title === chosenCheckpoint.title) ?
+                                        <InfoWindow
+                                            position={{ lat: chosenCheckpoint.lat, lng: chosenCheckpoint.lng }}
+                                        >
+                                            <div style={divStyle}>
+                                                <h2>Next: {chosenCheckpoint.title}</h2>
+                                                <h3>Estimated Arrival time: {convertToTimeString(chosenCheckpoint.arrivalExpected)}</h3>
+                                                <h3>Distance to go: {chosenCheckpoint.prevDistance / 1000} km</h3>
+                                            </div>
+                                        </InfoWindow>
+                                        : <></>
+                                }
 
-                        </Marker>
+                            </Marker>
+                        )
                     })}
 
 
