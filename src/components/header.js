3fec03149eb6cec7bfe7f6b8dd563dfbe5088e8f
unclechuen <33083306+unclechuen@users.noreply.github.com>
2022-06-18 01:21:45 +0800

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -11,7 +11,7 @@ export const fetch = async (dispatch, username, signedIn, userId) => {
     let data = await fetchUserFromJWT()
     if (data.username === undefined) {
         data.username = "";
-    }   
+    }
     data.signedIn = (data.username != "")
     dispatch(setUserInfo(data))
 }
@@ -46,66 +46,66 @@ const Header = () => {
     })
 
     return (
-       <>
-        <head>
-            <title>UOW FYP</title>
-        </head>
-                <header style={{ width: '100%' }}>
-            
+        <>
+            <head>
+                <title>UOW FYP</title>
+            </head>
+            <header style={{ width: '100%' }}>
 
-            <table style={{ width: '100%' }}>
-                <thead></thead>
-                <tbody>
-                    <tr>
-                        {/* Logo */}
-                        <td className="headerItem">
-                            <div>
-                                <Link href="/">
-                                    <a>
-                                        <img src="/images/logo.jpg" height="150" />
-                                    </a>
-                                </Link>
 
-                            </div>
-                        </td>
+                <table style={{ width: '100%' }}>
+                    <thead></thead>
+                    <tbody>
+                        <tr>
+                            {/* Logo */}
+                            <td className="headerItem">
+                                <div>
+                                    <Link href="/">
+                                        <a>
+                                            <img src="/images/logo.jpg" height="150" />
+                                        </a>
+                                    </Link>
 
-                        {/* Server status */}
-                        <td style={{ borderRadius: 20, textAlign: "center" }} className="headerItem">
-                            <div style={{ fontSize: 22, display: "inline-block", alignSelf: "flex-end", paddingLeft: 50, paddingRight: 50 }}>
-                                <div style={{ textAlign: "center" }} s>
-                                    <Badge color="gray" size="xl" radius="sm" variant="filled" >Server Status</Badge>
                                 </div>
-                                <br />
-                                <Badge color={backendIsUp ? "green" : "red"} style={{ backgroundColor: backendIsUp ? "#B6F3B8" : "#FCB2B2" }} size="xl" variant="dot">Backend Database</Badge><br />
-                                <Badge color={blockchainIsUp ? "green" : "red"} style={{ backgroundColor: blockchainIsUp ? "#B6F3B8" : "#FCB2B2" }} size="xl" variant="dot">Blockchain</Badge>
-                            </div>
+                            </td>
 
-                        </td>
+                            {/* Server status */}
+                            <td style={{ borderRadius: 20, textAlign: "center" }} className="headerItem">
+                                <div style={{ fontSize: 22, display: "inline-block", alignSelf: "flex-end", paddingLeft: 50, paddingRight: 50 }}>
+                                    <div style={{ textAlign: "center" }} s>
+                                        <Badge color="gray" size="xl" radius="sm" variant="filled" >Server Status</Badge>
+                                    </div>
+                                    <br />
+                                    <Badge color={backendIsUp ? "green" : "red"} style={{ backgroundColor: backendIsUp ? "#B6F3B8" : "#FCB2B2" }} size="xl" variant="dot">Backend Database</Badge><br />
+                                    <Badge color={blockchainIsUp ? "green" : "red"} style={{ backgroundColor: blockchainIsUp ? "#B6F3B8" : "#FCB2B2" }} size="xl" variant="dot">Blockchain</Badge>
+                                </div>
 
-                        {/* Login status */}
-                        <td className="headerItem">
-                            {
-                                (signedIn) ?
-                                    <div style={{ fontSize: 20, textAlign: 'right' }}>
-                                        Hi, {username}<br />
-                                        Role: {role}<br />
-                                        Company: {company}<br />
-                                        <Link href="#">
-                                            <Button
-                                                onClick={() => signOut()}>Logout</Button>
-                                        </Link>
-                                    </div> : <></>
-                            }
+                            </td>
 
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <hr />
-        </header>
+                            {/* Login status */}
+                            <td className="headerItem">
+                                {
+                                    (signedIn) ?
+                                        <div style={{ fontSize: 20, textAlign: 'right' }}>
+                                            Hi, {username}<br />
+                                            Role: {role}<br />
+                                            Company: {company}<br />
+                                            <Link href="#">
+                                                <Button
+                                                    onClick={() => signOut()}>Logout</Button>
+                                            </Link>
+                                        </div> : <></>
+                                }
+
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <hr />
+            </header>
 
         </>
-        
+
     )
 }
 
